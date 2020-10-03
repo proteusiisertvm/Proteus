6c25ddafed3ea3000a54a9e969b09209202d9e91
BREAD-123 <61983185+BREAD-123@users.noreply.github.com>
2020-10-04 00:29:35 +0530

    removed light mode from videos.html
    Not required

--- docs/Proteus-video/videos.js
+++ docs/Proteus-video/videos.js
@@ -10,12 +10,3 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
-
-const options = {
-  saveInCookies: true, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
-}
-
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
