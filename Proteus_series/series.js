0ac85c511322652e4cf38c9d6f3019e92a4f9bc7
BREAD-123 <61983185+BREAD-123@users.noreply.github.com>
2020-10-10 17:05:15 +0530

    testing like counter on series page
    This has only been added to the first blog post and can only fetch values, not write to the database.

--- Proteus_series/series.js
+++ Proteus_series/series.js
@@ -1,3 +1,21 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyApNi5rYeVsVkyzs_URaNRISwRu8TXdNZM",
+  authDomain: "proteus-blog-7bf97.firebaseapp.com",
+  databaseURL: "https://proteus-blog-7bf97.firebaseio.com",
+  projectId: "proteus-blog-7bf97",
+  storageBucket: "proteus-blog-7bf97.appspot.com",
+  messagingSenderId: "195466704947",
+  appId: "1:195466704947:web:95e707ea051c47f9a98b46",
+  measurementId: "G-P8VG30RTYF"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.database();
+const blog0001 =  document.getElementById('blog0001')
+const dblike = firebase.database().ref().child('blog0001');
+dblike.on('value', snap => {
+  blog0001.textContent = JSON.stringify(snap.val());
+})
 
 const options = {
   saveInCookies: true, // default: true,
