7464faeb9960d76969548d377fad7c15b5cdf29c
BREAD-123 <61983185+BREAD-123@users.noreply.github.com>
2020-10-16 21:38:26 +0530

    remade likes counter, made it localStorage dependent
    the database now stores the keys of people who have liked blog posts. All that is left to do is to connect the function to onClick of buttons

--- Proteus_series/series.js
+++ Proteus_series/series.js
@@ -14,36 +14,74 @@ firebase.initializeApp(firebaseConfig);
 database = firebase.database();
 
 function getLikesOnButton(bpid){
-  var bp = database.ref('blogposts/blog'+bpid+'/');
+  var bp = database.ref('blogposts/blog'+bpid+'/likes_count');
   var templikes = 0;
   bp.on('value', snap => {
-    templikes = snap.val().likes;
+    templikes = snap.val();
+    //console.log(snap.val());
     document.getElementById("blog"+bpid+"counter").innerText = templikes;
   });
   //for debugging purposes
   return templikes;
 }
 
+//if local storage doesnt have key
+if (localStorage.getItem("likey")===null){
+  //generate key
+  //console.log("run");
+  var acc = firebase.database().ref().child('Users').push().getKey();
+  var updates={};
+  updates[acc]="";
+  firebase.database().ref('Users/').push(updates);
+  localStorage.setItem("likey", acc);
+}
+
+//for all buttons
 getLikesOnButton("0001");
 getLikesOnButton("0002");
 
-//boo = whether button is liked
-/*
-function updateLikes(bpid, boo){
-  var bp = database.ref('blogposts/blog'+bpid+'/');
-  bp.transaction(function(post){
+
+function updateLikes(bpid){
+  var dbRef= firebase.database().ref("/blogposts/blog"+bpid+'/likes');
+  checkLiked(bpid, function(boo){
     if(boo){
-      bp.likes--;
+      firebase.database().ref("/blogposts/blog"+bpid+'/likes_count').transaction(function(value) {
+        return (value || 0) - 1;
+      });
+      console.log("removing");
+      firebase.database().ref("/blogposts/blog"+bpid+'/likes/'+localStorage["likey"]).remove();
     }
     else{
-      bp.likes++;
+      firebase.database().ref("/blogposts/blog"+bpid+'/likes_count').transaction(function(value) {
+        return (value || 0) + 1;
+      });
+      var updates = {};
+      updates[localStorage["likey"]]="";
+      firebase.database().ref("/blogposts/blog"+bpid+'/likes/').update(updates);
     }
-    return post;
   });
 }
 
-console.log(updateLikes("0001", false));
-*/
+//document.getElementById("b0001button").
+
+
+function checkLiked(bpid, callback){
+  var boo;
+  var bp = firebase.database().ref("/blogposts/blog"+bpid+"/likes/").child(localStorage["likey"]);
+  bp.once("value", snap => {
+    var obj = snap.val();
+    if (obj === null){
+      console.log("not liked");
+      boo = false;
+      callback(boo);
+    }
+    else{
+      console.log("liked");
+      boo= true;
+      callback(boo);
+    }
+  });
+}
 
 //darkmode
 const options = {
