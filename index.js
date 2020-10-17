32f48baf61dce6b7d9406e5cdf08ae05cbc6f984
Ritobrato Chatterjee <ritobrato97@gmail.com>
2020-10-17 22:35:26 +0530

    Update home page

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-const options = {
-  saveInCookies: true, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
-}
-
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
-
-// for tooltips
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+// AOS libarary
+AOS.init();
