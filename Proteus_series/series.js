6d4f6b193b5f4b9ee6543f842a400c50087fd33a
BREAD-123 <61983185+BREAD-123@users.noreply.github.com>
2020-10-21 00:33:21 +0530

    changed like buttons
    Softcoded initializing the likes counters, which will make it easier when adding new posts
    added color to them when liked, removes this color when unliked,

--- Proteus_series/series.js
+++ Proteus_series/series.js
@@ -47,6 +47,7 @@ function updateLikes(bpid){
       });
       //console.log("removing");
       //set to unliked heart
+      document.getElementById("b"+bpid+"heart").style.color = "#000000";
       firebase.database().ref("/blogposts/blog"+bpid+'/likes/'+localStorage["likey"]).remove();
     }
     else{
@@ -56,6 +57,7 @@ function updateLikes(bpid){
       var updates = {};
       updates[localStorage["likey"]]="";
       //set to liked heart
+      document.getElementById("b"+bpid+"heart").style.color = "#FF0000";
       firebase.database().ref("/blogposts/blog"+bpid+'/likes/').update(updates);
     }
   });
@@ -69,178 +71,32 @@ function checkLiked(bpid, callback){
     var obj = snap.val();
     if (obj === null){
       //console.log("not liked");
+
       boo = false;
       //set to unclicked heart
-
+      document.getElementById("b"+bpid+"heart").style.color = "#000000";
       callback(boo);
     }
     else{
       //console.log("liked");
+      document.getElementById("b"+bpid+"heart").style.color = "#FF0000";
       boo= true;
       //set to clicked heart
-
       callback(boo);
     }
   });
 }
 
-//to be completed
-
-//do for every button
-//blog post 1
-getLikesOnButton("0001");
-checkLiked("0001", function(boo){
-  if (boo){
-    //set to liked heart
+var arr = ["0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011", "0012"];
+arr.forEach(function initPost(bpid){
+    //console.log("init "+ bpid);
+    getLikesOnButton(bpid);
+    checkLiked(bpid, function(boo){});
+    document.getElementById("b"+bpid+"button").addEventListener("click", function(){
+      updateLikes(bpid);
+    });
   }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0001button").addEventListener("click", function(){
-  updateLikes("0001");
-});
-
-//blog post 2
-getLikesOnButton("0002");
-checkLiked("0002", function(boo){
-  if (boo){
-    //set to liked heart
-  }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0002button").addEventListener("click", function(){
-  updateLikes("0002");
-});
-
-//blog post 3
-getLikesOnButton("0003");
-checkLiked("0003", function(boo){
-  if (boo){
-    //set to liked heart
-  }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0003button").addEventListener("click", function(){
-  updateLikes("0003");
-});
-
-//blog post 4
-getLikesOnButton("0004");
-checkLiked("0004", function(boo){
-  if (boo){
-    //set to liked heart
-  }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0004button").addEventListener("click", function(){
-  updateLikes("0004");
-});
-
-//blog post 5
-getLikesOnButton("0005");
-checkLiked("0005", function(boo){
-  if (boo){
-    //set to liked heart
-  }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0005button").addEventListener("click", function(){
-  updateLikes("0005");
-});
-
-//blog post 6
-getLikesOnButton("0006");
-checkLiked("0006", function(boo){
-  if (boo){
-    //set to liked heart
-  }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0006button").addEventListener("click", function(){
-  updateLikes("0006");
-});
-
-//blog post 7
-getLikesOnButton("0007");
-checkLiked("0007", function(boo){
-  if (boo){
-    //set to liked heart
-  }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0007button").addEventListener("click", function(){
-  updateLikes("0007");
-});
-
-//blog post 8
-getLikesOnButton("0008");
-checkLiked("0008", function(boo){
-  if (boo){
-    //set to liked heart
-  }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0008button").addEventListener("click", function(){
-  updateLikes("0008");
-});
-
-//blog post 9
-getLikesOnButton("0009");
-checkLiked("0009", function(boo){
-  if (boo){
-    //set to liked heart
-  }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0009button").addEventListener("click", function(){
-  updateLikes("0009");
-});
-
-//blog post 10
-getLikesOnButton("0010");
-checkLiked("0004", function(boo){
-  if (boo){
-    //set to liked heart
-  }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0010button").addEventListener("click", function(){
-  updateLikes("0010");
-});
-
-//blog post 11
-getLikesOnButton("0011");
-checkLiked("0011", function(boo){
-  if (boo){
-    //set to liked heart
-  }
-  else{
-    //set to unliked heart
-  }
-});
-document.getElementById("b0011button").addEventListener("click", function(){
-  updateLikes("0011");
-});
-
+);
 
 
 //darkmode
