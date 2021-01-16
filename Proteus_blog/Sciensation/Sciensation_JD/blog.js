e9c1eed845b93f811a3b7c42b15a0b3e4cdbf3e2
Parth-Ankam <72865224+Parth-Ankam@users.noreply.github.com>
2021-01-16 19:02:31 +0530

    Blog update

---
+++ Proteus_blog/Sciensation/Sciensation_JD/blog.js
@@ -0,0 +1,8 @@
+const options = {
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
