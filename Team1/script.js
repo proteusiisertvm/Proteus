9451965163d600a3fa84fab32af85242261daf12
simar-25 <79756297+simar-25@users.noreply.github.com>
2021-09-19 01:46:08 +0530

    yooo
    team page changed format

--- Team1/script.js
+++ Team1/script.js
@@ -8,57 +8,260 @@ const persons = [
     social: {
 
     instagram: "https://www.instagram.com/vidyarashmihanehalli",
-    envelope: "https://www.gmail.com/vidyarashmi19@iisertvm.ac.in"
+    envelope: "mailto:vidyarashmi19@iisertvm.ac.in"
+    }
+  },
+  {
+    name: "Shreya Venkatesan",
+    photo: "../images/members1/Shreya.jpg",
+    title: "Vice President",
+    bio:
+      "<p>With a fondness for literature and biology, Shreya combines these via scientific writing. Her other interests include music, dance, atrocious puns, and constructive debates. However, the real love of her life will always be cats—a polarising opinion, for sure, but true love's course never did run smooth.</p>",
+    social: {
+    }
+  },
+  {
+    name: "Suhana Nujum G",
+    photo: "../images/members1/Suhana.jpg",
+    title: "Art Head",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      instagram: "https://www.instagram.com/suhana.xx",
+    }
+  },
+  {
+    name: "Riya Shende",
+    photo: "../images/members1/Riya.jpg",
+    title: "Wrtiter Head",
+    bio:
+      "<p>Riya Shende is a biology enthusiast, writer and co-author of ‘The Métamorphose’, an anthology featuring her poetry under the collection titled ‘Saudade’. She is a dancer, choreographer and international titleholder in Shito-Ryu Karate, with a proclivity for communicating in nature metaphors and sarcasm.</p>",
+    social: {
+      instagram: "https://www.instagram.com/riya_shende_",
+    }
+  },
+  {
+    name: "Shwetha Santhosh",
+    photo: "../images/members1/Shwetha.jpeg",
+    title: "Finance Head",
+    bio:
+      "<p>Shwetha Santhosh is an active member of the Proteus Events team. She is always ready for a challenge and  doesn’t quit until she believes she has done justice to the task. She makes the best of what is given to her and considers every opportunity an experience worth learning from. Versatile, easygoing and resourceful- she truly is a wonderful addition to the team.</p>",
+    social: {
+      instagram: "https://www.instagram.com/shwetha._s",
+    }
+  },
+  {
+    name: "Parth Ankam",
+    photo: "../images/members1/Parth.jpg",
+    title: "Merch Head",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      instagram: "https://www.instagram.com/parth_ankam",
+    }
+  },
+  {
+    name: "Goutami Nayak",
+    photo: "../images/members1/Goutami.jpg",
+    title: "Social Team Head",
+    bio:
+    "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      instagram: "https://www.instagram.com/goutamin",
+    }
+  },
+  {
+    name: "Sneha PR",
+    photo: "../images/members1/Sneha.jpg",
+    title: "Event Head",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      instagram: "https://www.instagram.com/nehap_r",
+    }
+  },
+  {
+    name: "Arunima Mathew",
+    photo: "../images/members1/Arunima.jpg",
+    title: "Art Vice Head",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      instagram: "https://www.instagram.com/nemo_scape",
+    }
+  },
+  {
+    name: "Aaqilah A J",
+    photo: "../images/members1/Aaqilah.jpg",
+    title: "Writer Vice Head",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+      instagram: "https://www.instagram.com/aaqilah_711",
+    }
+  },
+  {
+    name: "Anagha Muralidharan",
+    photo: "../images/members1/Anagha.jpg",
+    title: "Finance Vice Head",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+      instagram: "https://www.instagram.com/_._._anagha_._._",
+    }
+  },
+  {
+    name: "Akhila S",
+    photo: "../images/members1/Akhila.jpg",
+    title: "Merch Vice Head",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+    }
+  },
+  {
+    name: "Avani Kajarekar",
+    photo: "../images/members1/Avani.jpg",
+    title: "Writer Vice Head",
+    bio:
+      "<p>Avani, she likes biology and especially genetics! Though she is not very good at art, she loves to do something creative. Apart from that, she is fond of acting, dancing, music and not to forget, photography. She also loves all sort of animals.</p>",
+    social: {
+      instagram: "https://www.instagram.com/un.knownkinda.me",
+    }
+  },
+  {
+    name: "Harshini S",
+    photo: "../images/members1/Harshini.jpg",
+    title: "Event Vice Head",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+      instagram: "https://www.instagram.com/harshinisivagnanam",
     }
   },
   {
     name: "Simarpreet Kaur",
-    photo: "../images/members/Simar.png",
-    title: "Developer",
+    photo: "../images/members1/Simar.jpg",
+    title: "Tech Team",
     bio:
-      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
     social: {
-      facebook: "#",
-      twitter: "https://twitter.com/knyttneve",
-      linkedin: "#"
+      instagram: "https://www.instagram.com/_simar25",
     }
   },
   {
-    name: "Carl Roland",
-    photo: "https://serving.photos.photobox.com/2226093445b640ea69b3247d4e4a31ee16d7569a38f898affce33adc8fc8d0f3ecf79591.jpg",
-    title: "Developer",
+    name: "Ashwin Nair",
+    photo: "../images/members1/Ashwin.png",
+    title: "Tech Team",
     bio:
-      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
     social: {
-      facebook: "#",
-      twitter: "https://twitter.com/knyttneve",
-      linkedin: "#"
+      instagram: "https://www.instagram.com/_ashwiinnn",
     }
   },
   {
-    name: "Paul Wilson",
-    photo: "https://serving.photos.photobox.com/80553616a29779bd113f7a340b9b1eae3636dd38534c57877597a17b7131be741c67b3ae.jpg",
-    title: "UI Designer",
+    name: "Sarthak Thorat",
+    photo: "../images/members1/Sarthak.jpg",
+    title: "Tech Team",
     bio:
-      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
     social: {
-      facebook: "#",
-      twitter: "https://twitter.com/knyttneve",
-      linkedin: "#"
+      instagram: "https://www.instagram.com/sarthakthorat_",
     }
   },
   {
-    name: "Alice Jenkins",
-    photo: "https://serving.photos.photobox.com/52898788b03c8a0e32a8cb52b4d43d7525f119daa2629569dbda0a8d827192217fb64c22.jpg",
-    title: "QA Engineer",
+    name: "Adam Zacharia Anil",
+    photo: "../images/members1/Adam.jpg",
+    title: "Social Team",
     bio:
-      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
     social: {
-      facebook: "#",
-      twitter: "https://twitter.com/knyttneve",
-      linkedin: "#"
+      instagram: "https://www.instagram.com/adam_zacharia_anil",
     }
-  }
+  },
+  {
+    name: "Gopal Kulkarni",
+    photo: "../images/members1/Gopal.jpg",
+    title: "Social Team",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+      instagram: "https://www.instagram.com/gopalkulkarni22",
+    }
+  },
+  {
+    name: "Deopal Birua",
+    photo: "../images/members1/Deo.png",
+    title: "Social Team",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+      instagram: "https://www.instagram.com/deopal_",
+    }
+  },
+  {
+    name: "Srinandhitha",
+    photo: "../images/members1/Srinandhitha.jpg",
+    title: "Social Team",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+      instagram: "https://www.instagram.com/sri_nandhitha",
+    }
+  },
+  {
+    name: "Barninee Sengupta",
+    photo: "../images/members1/Barninee.jpg",
+    title: "Illustrator",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+      instagram: "https://www.instagram.com/hurricandrizzles",
+    }
+  },
+  {
+    name: "Sandra Francis",
+    photo: "../images/members1/Sandra.jpg",
+    title: "Writer",
+    bio:
+      "<p>Sandra is a person having a deep interest in biology and academic research. Reading is her favorite pastime. She also finds her interest in music and craft-making. Her compassionate heart always treats every life with devotion, and so is her love for raising pets and plants.</p>",
+    social: {
+    }
+  },
+  {
+    name: "Ashley Roby",
+    photo: "../images/members1/Ashley.jpg",
+    title: "Writer",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+      instagram: "https://www.instagram.com/ash_rises",
+    }
+  },
+  {
+    name: "Abhimanyu S M",
+    photo: "../images/members1/Abhimanyu.jpg",
+    title: "Events Team",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+      instagram: "https://www.instagram.com/abhimanyu_sm",
+    }
+  },
+  {
+    name: "Pragya Balot",
+    photo: "../images/members1/Pragya.jpg",
+    title: "Events Team",
+    bio:
+      "<p>Aaqilah, analogous to her name, prefers to stand out in a group. She can be quite introverted but not around the people she fraternises with. She is a curious soul that is often intrigued by bizarre things. Writing is one of the many things that spark her interest. Owing to her love and appreciation for the field of medicine, she aspires to become a pharmaceutical scientist.</p>",
+    social: {
+      instagram: "https://www.instagram.com/pragya_17_balot",
+    }
+  },
+
+
+
+
 ];
 
 const app = new Vue({
